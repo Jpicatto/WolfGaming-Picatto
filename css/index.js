040c05b3a269a18944805c60da1c45efcd297dc0
Jpicatto <javierpicatto2013@gmail.com>
2022-09-09 17:51:57 -0300

    responsive_correcciones

--- css/index.js
+++ css/index.js
@@ -1,6 +1,6 @@
-const navToggle = document.querySelector(".nav-toggle")
-const navMenu = document.querySelector(".nav-menu")
+const navToggle1 = document.querySelector(".nav-toggle1")
+const navMenu1 = document.querySelector(".nav-menu1")
 
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible");
+navToggle1.addEventListener("click", () => {
+    navMenu1.classList.toggle("nav-menu1_visible");
 });
